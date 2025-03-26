09b0bdb52dfc8bf433e39acb5ffa7dcaefb192e1
Ifeanyi Iheme <iheme.ifeanyi@yahoo.com>
2025-03-26 14:28:44 +0100

    Delete utils directory

--- utils/FormatString.tsx
+++
@@ -1,8 +0,0 @@
-export const formattedItem = (item: string) => {
-  return item.toLowerCase().replace(/\s+/g, "-");
-};
-
-export function capitalizeWord(word: string): string {
-  if (!word) return "";
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
